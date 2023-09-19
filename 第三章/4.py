a066a0a43b2bd4a9223efa5784f140f5e676adb0
Ayaka <2504970020@qq.com>
2023-09-19 19:26:03 +0800

    .

---
+++ 第三章/4.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# 创建一个空链表
+my_list = LinkedList()
+
+# 向链表添加元素
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+
+# 显示链表内容
+my_list.display()
