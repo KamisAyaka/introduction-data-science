f6047d2fbc7a8fb19e58afd8149cc7bab2dec497
Ayaka <2504970020@qq.com>
2023-09-21 23:55:37 +0800

    .

--- 第三章/4.py
+++ 第三章/4.py
@@ -17,20 +17,57 @@ class LinkedList:
             current = current.next
         current.next = new_node
 
-    def display(self):
+    def search(self,data):
+        if not self.head:
+            print("error")
+            return
         current = self.head
-        while current:
-            print(current.data, end=" -> ")
+        while current.data != data and current.next is not None:
+                current=current.next
+        if current.data == data:print("yes")
+        else:print("error")
+        return
+    def delete(self,data):
+        if not self.head:
+            print("error")
+            return
+        current = self.head
+        if current.data == data and current.next is not None:
+            self.head = current.next
+            return
+        while current.next is not None:
+            if current.next.data != data:
+                current = current.next
+        if current.next.data == data and current.next.next is not None:
+            current.next = current.next.next
+        elif current.next.next is None:
+            current = None
+        else:
+            print("error")
+        return
+    def change(self,data1,data2):
+        if not self.head:
+            print("error")
+            return
+        current = self.head
+        while current.data != data1 and current.next is not None:
             current = current.next
-        print("None")
-
-# 创建一个空链表
+        if current.data == data1:
+            current.data = data2
+        else:
+            print("error")
+        return
+    # 创建一个空链表
 my_list = LinkedList()
 
 # 向链表添加元素
 my_list.append(1)
 my_list.append(2)
 my_list.append(3)
-
-# 显示链表内容
-my_list.display()
+my_list.search(2)
+my_list.change(3,4)
+my_list.delete(1)
+curr = my_list.head
+while curr :
+    print(curr.data)
+    curr=curr.next
